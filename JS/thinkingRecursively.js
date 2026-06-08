c787fc5f328d5f501185b28e64d54ed1be8d007b
dm-lalwani <mr.d.m.lalwani@gmail.com>
2026-06-09 00:42:30 +0530

    revision

--- JS/thinkingRecursively.js
+++ JS/thinkingRecursively.js
@@ -1,4 +1,34 @@
+// version 1
 let user = {
+  name: "Dinesh",
+  address: {
+    city: "Nashik",
+    area: "Shahu Nagar",
+  },
+  office: {
+    city: "Pune",
+    area: {
+      landmark: "Baner",
+    },
+  },
+};
+
+let finalObj = {};
+
+function flattenObj(obj, parent) {
+  for (let key in obj) {
+    if (typeof obj[key] === "object") {
+      flattenObj(obj[key], parent + "_" + key);
+    } else {
+      finalObj[parent + "_" + key] = obj[key];
+    }
+  }
+}
+
+flattenObj(user, "user");
+// console.log(finalObj);
+
+let user2 = {
   name: "Dinesh",
   address: {
     city: "Nashik",
@@ -19,20 +49,21 @@ let user = {
   createdAt: new Date(),
 };
 
-let userChild = Object.create(user);
+let userChild = Object.create(user2);
 
 userChild.lastName = "Lalwani";
 
-let finalObj = {};
+let finalObj2 = {};
 function flattenObject(obj, parent) {
-  for (const key of Object.keys(obj)) {
+  // for (const key of Object.keys(obj)) {
+  for (let key in obj) {
     // if (obj[key] == null) {
     //   finalObj[parent + "_" + key] = "";
     //   continue;
     // }
-    // if (!obj.hasOwnProperty(key)) continue;
+    if (!obj.hasOwnProperty(key)) continue;
     if (typeof obj[key] === "function") {
-      finalObj[parent + "_" + key] = obj[key].name;
+      finalObj2[parent + "_" + key] = obj[key].name;
       continue;
     }
     if (
@@ -41,9 +72,9 @@ function flattenObject(obj, parent) {
       !Array.isArray(obj[key])
     )
       flattenObject(obj[key], parent + "_" + key);
-    else finalObj[parent + "_" + key] = obj[key];
+    else finalObj2[parent + "_" + key] = obj[key];
   }
 }
 
 flattenObject(userChild, "userChild");
-console.log(finalObj);
+console.log(finalObj2);
